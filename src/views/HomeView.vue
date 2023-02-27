c611ba51552c874ffc9b4400bb9209ba4cd050f5
longkma <levanlongkma@gmail.com>
2023-02-27 17:48:09 +0700

    init project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <i class="t2ico t2ico-wallet"></i>
   </div>
 </template>
 
