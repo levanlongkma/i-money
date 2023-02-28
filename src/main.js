47a3e8c709e6f392c4bf3d837f62b7c15d286f5b
longkma <levanlongkma@gmail.com>
2023-02-28 16:23:04 +0700

    message

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/styles/tailwind.css";
 import "./assets/styles/style.css";
 
 createApp(App).use(router).mount("#app");
